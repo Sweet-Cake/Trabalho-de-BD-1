29b94b954336ac45c08e8223902208962b2a3656
Cake <melissaqgaspar@gmail.com>
2018-04-16 16:55:43 -0300

    Add files via upload

--- EscolaSamba/src/controle/controleWeb.java
+++ EscolaSamba/src/controle/controleWeb.java
@@ -13,6 +13,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import Dao.Inserir;
+import Dao.Lista;
 import model.*;
 
 /**
@@ -42,7 +43,11 @@ public class controleWeb extends HttpServlet {
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		Inserir inserir=new Inserir();
-		String msg;
+		Lista lista = new Lista();
+		String msg = request.getParameter("cmd");
+		System.out.println(msg);
+		switch (msg.toLowerCase()){
+		case "inserir":
 			Escola es=new Escola();
 			Quesito qs= new Quesito();
 			Jurado jr=new Jurado();
@@ -52,25 +57,31 @@ public class controleWeb extends HttpServlet {
 			jr.setNome(request.getParameter("Jurado"));	
 			try{
 				nt.setNota(Double.parseDouble(request.getParameter("Nota")));
-				System.out.println("puchi");
+				//System.out.println("puchi");
+				if ((nt.getNota()>=5) && (nt.getNota()<=10)){
 				try {
 					inserir.insert(es, qs, jr, nt);
-					System.out.println("Passei por aqui"); 
+					//System.out.println("Passei por aqui"); 
 					response.setContentType("text/plain");
 					Double nota=Double.parseDouble(request.getParameter("Nota"));
 					PrintWriter sair = response.getWriter();
-				    sair.print("NOTA ADICIONADA COM SUCESSO!"+nota);
+				    sair.print("NOTA ADICIONADA:");
 					/*msg = String.format("Nota para a escola %s inserida com sucesso!", es.nome);
 					request.getSession().setAttribute("MENSAGEM", msg);
 					response.sendRedirect("./index.jsp?");*/
 				} catch (SQLException e) {
 					response.setContentType("text/plain");
 					PrintWriter sair2 = response.getWriter();
-				    sair2.print("NOTA Já ADICIONADA");
+				    sair2.print("NOTA JÁ ADICIONADA");
 					//msg = String.format("Nota já inserida");
 					//request.getSession().setAttribute("MENSAGEM", msg);
 					e.printStackTrace();
 					//response.sendRedirect("./index.jsp");
+				}}
+				else {
+					response.setContentType("text/plain");
+					PrintWriter sair3 = response.getWriter();
+				    sair3.print("Valor fora do intervalo");
 				}
 			}
 			catch( NumberFormatException e){
@@ -79,5 +90,36 @@ public class controleWeb extends HttpServlet {
 				PrintWriter sair3 = response.getWriter();
 			    sair3.print("Isso Não é um Numero, eu sei");
 			}
+			break;
+		case "total":
+			try {
+				msg = lista.listaTotal();
+			} catch (SQLException e1) {
+				// TODO Auto-generated catch block
+				e1.printStackTrace();
+			}
+			PrintWriter sair = response.getWriter();
+		    sair.print(msg);
+		    break;
+		case "mostrar":
+			try {
+				msg = lista.listaNotas(Integer.parseInt(request.getParameter("Quesito")));
+			} catch (SQLException e1) {
+				// TODO Auto-generated catch block
+				e1.printStackTrace();
+			}
+			sair = response.getWriter();
+		    sair.print(msg);
+			break;
+		default:
+			System.out.println("Nothin' to see here, kiddo");
+			break;
+		}
+		
+		/*
+		 * 
+		 */
+		
+			
 		}
 }
